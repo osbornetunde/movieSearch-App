cc08b97371d5b006e6ca8987e16541fda4fbde85
Tunde Osborne <osbornetunde@gmail.com>
2018-10-10 00:44:51 +0100

    code refactoring

---
+++ program.py
@@ -0,0 +1,36 @@
+import movieSearch
+
+def main():
+	print_header()
+	search_event_loop()
+
+
+def print_header():
+	print('--------------------------------')
+	print('          Movie Search')
+	print('--------------------------------')
+
+
+
+
+def search_event_loop():
+	search = 'ONCE_THROUGH_LOOP'
+
+	while search != 'x':
+		search = input('Movie search text (x to exit): ')
+		if search != 'x':
+			results = movieSearch.find_movies(search)
+			print("Found {} results.".format(len(results)))
+			for r in results:
+				print('{} -- {}'.format(r.year, r.title))
+			print()
+
+	print('exiting....')
+
+
+
+
+
+
+if __name__ == '__main__':
+	main()
